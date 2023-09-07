001cda471f7152dbd22c844f6e6856bd8a173cb0
Daniel Vadil <dvadz@yahoo.com>
2023-09-07 17:10:28 -0400

    Ceated a redux slice for suers

---
+++ src/store/slices/usersSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const usersSlice = createSlice({
+  name: "user",
+  initialState: { data: [] },
+  reducers: {},
+});
+
+export const usersReducer = usersSlice.reducer;
