e2dee97cd078a51a1a0a2aad91df794346740102
Daniel Vadil <dvadz@yahoo.com>
2023-09-07 17:17:57 -0400

    Created the redux store

---
+++ src/store/index.js
@@ -0,0 +1,6 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { usersReducer } from "./slices/usersSlice";
+
+const store = configureStore({ reducer: { users: usersReducer } });
+
+export default store;
