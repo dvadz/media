d31bad41251d55d793e413e1df4d2fca6c1a7b30
Daniel Vadil <dvadz@yahoo.com>
2023-09-09 23:20:26 -0400

    Use useEffect on UseList component to dispatch fetchUser

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchUsers } from "../store";
+
 const UsersList = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]); //[dispatch] does not serve a purpose except to get rid of the eslint warning
+
   return <div>UsersList</div>;
 };
 
