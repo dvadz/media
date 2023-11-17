699bb61d9bda094e38b69430206dc64cfdafbc5a
Daniel Vadil <dvadz@yahoo.com>
2023-11-17 15:10:08 -0500

    Added a pause function that adds a delay to the thunk so that we can see the spinner that we will build soon

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -3,7 +3,16 @@ import axios from "axios";
 
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
   const response = await axios.get("http://localhost:3003/users");
+
+  await pause(1000);
+
   return response.data;
 });
+
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
 
 export { fetchUsers };
