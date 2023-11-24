52161c45e629fb26324372f2ee72f198e37192ff
Daniel Vadil <dvadz@yahoo.com>
2023-11-24 17:20:36 -0500

    Added animation and styling to skeleton loader

--- src/components/Skeleton.js
+++ src/components/Skeleton.js
@@ -1,6 +1,26 @@
 import classNames from "classnames";
 
-const Skeleton = ({ times }) => {
+const Skeleton = ({ times, className }) => {
+  const outClassNames = classNames(
+    "relative",
+    "overflow-hidden",
+    "bg-gray-200",
+    "rounded",
+    "mb-2.5",
+    className
+  );
+  const innerClassNames = classNames(
+    "animate-shimmer",
+    "absolute",
+    "inset-0",
+    "-translate-x-full",
+    "bg-gradient-to-r",
+    "from-gray-200",
+    "via-white",
+    "to-gray-200",
+    className
+  );
+
   //   const boxes = [];
   //   for (let i = 0; i < times; i++) {
   //     boxes.push(<div key={i} />);
@@ -10,7 +30,11 @@ const Skeleton = ({ times }) => {
   const boxes = Array(times)
     .fill(0)
     .map((_, i) => {
-      return <div key={i}>skeleton</div>;
+      return (
+        <div key={i} className={outClassNames}>
+          <div className={innerClassNames}></div>
+        </div>
+      );
     });
 
   return boxes;
