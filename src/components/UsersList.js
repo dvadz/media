087f92261caeed72cd1ab2e31ccb77cc01a86872
Daniel Vadil <dvadz@yahoo.com>
2023-12-23 01:33:17 -0500

    Turned useThunk into a custom hook

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,34 +1,9 @@
-import { useEffect, useState, useCallback } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store";
 import Button from "./Button";
 import Skeleton from "./Skeleton";
-
-const useThunk = (thunk) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  const dispatch = useDispatch();
-
-  const runThunk = useCallback(
-    (arg) => {
-      setIsLoading(true);
-      dispatch(thunk(arg))
-        .unwrap()
-        .catch((err) => {
-          setError(err).finally(() => {
-            setIsLoading(false);
-          });
-        })
-        .finally(() => {
-          setIsLoading(false);
-        });
-    },
-    [dispatch, thunk]
-  );
-
-  return [runThunk, isLoading, error];
-};
+import useThunk from "./hooks/use-thunk";
 
 const UsersList = () => {
   const [doFetchUsers, isLoadingUsers, loadingUsersError] =
