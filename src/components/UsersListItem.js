883bdfdf7c9c9915011626a6fefd99a561bd3b68
Daniel Vadil <dvadz@yahoo.com>
2023-12-30 16:09:10 -0500

    Added delete button

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -1,8 +1,23 @@
+import { removeUser } from "../store";
+import useThunk from "./hooks/use-thunk";
+import Button from "./Button";
+import { GoTrash } from "react-icons/go";
+
 const UsersListItem = ({ user }) => {
+  const [doRemoveUser, isLoading, error] = useThunk(removeUser);
+
+  const handleClick = () => {
+    doRemoveUser(user);
+  };
+
   return (
-    <div key={user.id} className="mb-2 border rounded flex items-center">
+    <div className="mb-2 border rounded flex items-center">
+      <Button loading={isLoading} onClick={handleClick}>
+        <GoTrash />
+      </Button>
       <div className="flex p-2 justify-between items-center cursor-pointer">
         {user.name}
+        {error ? <div>Error deleting user</div> : null}
       </div>
     </div>
   );
