158d1460ca880010cd2d2aeca210e8330bb06ccc
Daniel Vadil <dvadz@yahoo.com>
2024-01-18 18:27:52 -0500

    Linked the new RTK Query API to the store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { usersReducer } from "./slices/usersSlice";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
+import { albumsApi } from "./apis/albumsApi";
 
-const store = configureStore({ reducer: { users: usersReducer } });
+const store = configureStore({
+  reducer: {
+    users: usersReducer,
+    [albumsApi.reducerPath]: albumsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(albumsApi.middleware);
+  },
+});
+
+setupListeners(store.dispatch);
 
 export default store;
 export * from "./thunks/fetchUsers";
 export * from "./thunks/addUser";
 export * from "./thunks/removeUser";
+export * from "./apis/albumsApi";
