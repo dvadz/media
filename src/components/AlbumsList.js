3cd92ea52065ea308eca1ed9c00d3bf0845b1ede
Daniel Vadil <dvadz@yahoo.com>
2024-01-19 21:22:17 -0500

    Render an ExpandablePanel for each album

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,5 +1,32 @@
+import { useFetchAlbumsQuery } from "../store";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+
 const AlbumsList = ({ user }) => {
-  return <div> Albums by {user.name}</div>;
+  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+
+  let content;
+
+  if (isLoading) {
+    content = <Skeleton times={3} className="h-5" />;
+  } else if (error) {
+    content = <div>{error}</div>;
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          list of photos here
+        </ExpandablePanel>
+      );
+    });
+    return (
+      <div>
+        <div>Albums by {user.name}</div>
+        <div>{content}</div>
+      </div>
+    );
+  }
 };
 
 export default AlbumsList;
