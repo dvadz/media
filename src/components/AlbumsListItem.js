2d35046038e158794fecdf3e39b37f733230e2f4
Daniel Vadil <dvadz@yahoo.com>
2024-01-23 18:47:00 -0500

    Added a delete button

--- src/components/AlbumsListItem.js
+++ src/components/AlbumsListItem.js
@@ -1,7 +1,16 @@
+import { GoTrash } from "react-icons/go";
+import Button from "./Button";
 import ExpandablePanel from "./ExpandablePanel";
 
 const AlbumsListItem = ({ album }) => {
-  const header = <div>{album.title}</div>;
+  const header = (
+    <div>
+      <Button>
+        <GoTrash />
+      </Button>
+      {album.title}
+    </div>
+  );
   return (
     <ExpandablePanel key={album.id} header={header}>
       list of photos here
