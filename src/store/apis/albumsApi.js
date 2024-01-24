a2b683e3fdf8448031ec81b31a6c6b4ec1aeacb6
Daniel Vadil <dvadz@yahoo.com>
2024-01-24 00:50:54 -0500

    Added endpoint for deleting albums

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -19,6 +19,17 @@ const albumsApi = createApi({
   }),
   endpoints(builder) {
     return {
+      removeAlbum: builder.mutation({
+        invalidatesTags: (result, error, album) => {
+          return [{ type: "Album", id: album.userId }];
+        },
+        query: (album) => {
+          return {
+            url: `/albums/${album.id}`,
+            method: "DELETE",
+          };
+        },
+      }),
       addAlbum: builder.mutation({
         invalidatesTags: (result, error, user) => {
           return [{ type: "Album", id: user.id }];
@@ -50,5 +61,9 @@ const albumsApi = createApi({
   },
 });
 
-export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
+export const {
+  useFetchAlbumsQuery,
+  useAddAlbumMutation,
+  useRemoveAlbumMutation,
+} = albumsApi;
 export { albumsApi };
