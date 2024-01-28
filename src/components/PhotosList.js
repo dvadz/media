075f9c631ae6ae0b1554c1c028653043f4e78a05
Daniel Vadil <dvadz@yahoo.com>
2024-01-27 22:26:24 -0500

    Set content of PhotosList

--- src/components/PhotosList.js
+++ src/components/PhotosList.js
@@ -1,5 +1,20 @@
+import { useFetchPhotosQuery } from "../store";
+
 const PhotosList = ({ album }) => {
-  return <div>{album.title}</div>;
+  const { data, error, isFetching } = useFetchPhotosQuery(album);
+
+  let content;
+  if (isFetching) {
+    content = "Fetching";
+  } else if (error) {
+    content = "ERROR";
+  } else {
+    content = data.map((photo) => {
+      return <div> {photo.url}</div>;
+    });
+  }
+
+  return <div>{content}</div>;
 };
 
 export default PhotosList;
