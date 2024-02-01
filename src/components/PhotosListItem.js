c4506a50d49bdf41fe951526b32fe8f4d43823bc
Daniel Vadil <dvadz@yahoo.com>
2024-01-31 23:15:53 -0500

    Added photo remove button

--- src/components/PhotosListItem.js
+++ src/components/PhotosListItem.js
@@ -1,7 +1,23 @@
+import { GoTrash } from "react-icons/go";
+import { useRemovePhotoMutation } from "../store";
+
 const PhotosListItem = ({ photo }) => {
+  const [removePhoto] = useRemovePhotoMutation();
+
+  const handleRemovePhoto = () => {
+    removePhoto(photo);
+  };
+
   return (
-    <div className="m-2">
-      <img src={photo.url} alt="random pix" />
+    <div className="relative cursor-pointer m-2">
+      <img src={photo.url} className="h-20 w-20" alt="random pix" />
+      {/* <div className="absolute inset-0 flex items-center justify-center opacity-0 hover:bg-gray-200 hover:opacity-80"> */}
+      <div className="absolute inset-0 flex items-center justify-center opacity-0 hover:bg-gray-200 hover:opacity-80">
+        <GoTrash
+          className="text-5xl text-red-800"
+          onClick={handleRemovePhoto}
+        />
+      </div>
     </div>
   );
 };
